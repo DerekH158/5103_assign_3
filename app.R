85d7671dfe0b1d0f283d0df3ba9c42537e2bf870
DerekH158 <69539233+DerekH158@users.noreply.github.com>
2022-03-30 15:48:48 -0400

    Update app.R

--- app.R
+++ app.R
@@ -10,7 +10,7 @@
 #assignment #3
 #created by Derek Halkes
 #version 3
-#last modified date: 3/28/2022
+#last modified date: 3/30/2022
 
 #libraries needed
 library(tidyverse)
@@ -25,16 +25,16 @@ bees <- read_csv("honeybees.csv")
 
 #removing unneeded columns
 bees2 <- bees[c(1:10)]
-
+bees2
 #reordering 2 of the columns
 bees2 <- bees2[with(bees2, order(StateName, year)), ]
 
 #renaming columns
 bees3 <- rename(bees2, c("No. of colonies" = "numcol",
-                "Yield per colony" = "yieldpercol",
-                "Total production" = "totalprod",
-                "Price per lb." = "priceperlb",
-                "Production value" = "prodvalue"))
+                         "Yield per colony" = "yieldpercol",
+                         "Total production" = "totalprod",
+                         "Price per lb." = "priceperlb",
+                         "Production value" = "prodvalue"))
 
 #changing State to lower case for joins
 #bees$StateName = tolower(bees$StateName)
@@ -44,51 +44,50 @@ bees3 <- rename(bees2, c("No. of colonies" = "numcol",
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
+    
     # Application title
     titlePanel("Honeybee data in the US"),
-
-sidebarLayout(
     
-sidebarPanel(
-    
-    # Create a new Row in the UI for selectInputs
-    fluidRow(
-        column(4,
-               selectInput("stt",
-                           "State:",
-                           c("All",
-                             unique(as.character(bees3$StateName))))
-        )),
-    fluidRow(
-        column(4,
-               selectInput("year",
-                           "Year:",
-                           c("All",
-                             unique(as.character(bees3$year))))
+    sidebarLayout(
+        
+        sidebarPanel(
+            
+            # Create a new Row in the UI for selectInputs
+            fluidRow(
+                column(4,
+                       selectInput("stt",
+                                   "State:",
+                                   c("All",
+                                     unique(as.character(bees3$StateName))))
+                )),
+            fluidRow(
+                column(4,
+                       selectInput("year",
+                                   "Year:",
+                                   c("All",
+                                     unique(as.character(bees3$year))))
+                )
+            )
         ),
-    ),
-),
-mainPanel(
-    # Create a new row for the table.
-   DT::dataTableOutput("table")
-    
-,
-#spot for barplot 1
-    plotOutput("beeplot1")  
-
-#secondary barplot
-    plotOutput("beeplot2")
-
+        mainPanel(
+            # Create a new row for the table.
+            DT::dataTableOutput("table"),
+            
+            #spot for barplot 1
+            plotOutput("beeplot1"),  
+            
+            #secondary barplot
+            plotOutput("beeplot2")
+            
+        )
+    )
 )
 
-))
-
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
-#table output
-    output$table <- DT::renderDataTable(DT::datatable({
+server <- function(input, output, session) {
+    
+    #table output
+    output$table <- DT::renderDataTable({
         data <- bees3
         if (input$stt != "All") {
             data <- data[data$StateName == input$stt,]
@@ -97,27 +96,52 @@ server <- function(input, output) {
             data <- data[data$year == input$year,]
         }
         data
-    }))
-    
-#barplot
-   output$beeplot1 <- renderPlot({
         
-#render barplot
-        barplot(bees2[,inputtotalprod], 
-                main=input$totalprod,
-                ylab="Total bee production",
-                xlab="Year")}
-
-#barplot #2
-    output$beeplot2 <- renderPlot({
-#secondary barplot
-plot(x = year, y = bees2$priceperlb,
-     main="Price per pound by year",
-     xlab="Year",
-     ylab="Price per lb.",
-     col="blue")}
-            
+    })
     
+    #barplot
+    output$beeplot1 <- renderPlot({ # FIXED
+        data <- bees3
+        if (input$stt != "All") {
+            data <- data[data$StateName == input$stt,]
+        }
+        if (input$year != "All") {
+            data <- data[data$year == input$year,]
+        }
+        
+        #render barplot
+        boxplot(`Total production` ~ year, data=data,
+                main="Total Production",  #input$totalprod,
+                ylab="Total bee production",
+                xlab="Year")
+    })
+    
+    #barplot #2
+    output$beeplot2 <- renderPlot({ # FIXED using ggplot
+        
+        data <- bees3
+        if (input$stt != "All") {
+            data <- data[data$StateName == input$stt,]
+        }
+        if (input$year != "All") {
+            data <- data[data$year == input$year,]
+        }
+        
+        #secondary barplot
+        #    barplot(x = year, y = bees2$priceperlb,
+        # main="Price per pound by year",
+        # xlab="Year",
+        # ylab="Price per lb.",
+        # col="blue")
+        ggplot(data=data, aes(x = factor(year), y=`Price per lb.`)) +
+            geom_boxplot(colour = "blue") +
+            labs(title = paste0("Price per pound by ",input$year),
+                 x="Year",
+                 y=paste0("Price per lb.", input$stt))
+    })
+    
+}     
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
